d2f52bafdf57ef6fab71d57630f8503126ed0c88
origin <islam.kamal@eg.aictec.com>
2023-11-15 13:32:49 +0200

    redirect to google play or apple store directly depend on platform

--- lib/view/action_screen.dart
+++ lib/view/action_screen.dart
@@ -2,9 +2,6 @@ import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:web_firebase_app/base/Helper/app_event.dart';
-
-import '../logic/public_scan_bloc.dart';
 
 class ActionScreen extends StatefulWidget{
   final String qrCode;
@@ -13,7 +10,6 @@ class ActionScreen extends StatefulWidget{
   State<StatefulWidget> createState() {
     return ActionScreenState();
   }
-
 }
 class ActionScreenState extends State<ActionScreen> {
   String platform = "";
@@ -21,8 +17,16 @@ class ActionScreenState extends State<ActionScreen> {
   void initState() {
 
       platform = getOSInsideWeb();
-    print("platform : ${platform}");
-
+      switch(platform){
+        case "Android":
+          window.open('https://play.google.com/store/apps/details?id=com.qara.hse', 'new tab');
+          break;
+        case 'ios':
+          window.open('https://apps.apple.com/eg/app/hse-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC-%D8%AD%D9%88%D8%A7%D9%81%D8%B2-%D8%A7%D9%84%D8%B3%D9%88%D9%8A%D8%AF%D9%89/id6444411065', 'new tab');
+          break;
+        default:
+          break;
+      }
 
     String data ="";
     window.history.replaceState(data, "", data);
@@ -42,7 +46,7 @@ class ActionScreenState extends State<ActionScreen> {
 
     return Scaffold(
       body: Center(
-        child: Column(
+        child: platform == "Web" ? Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -62,9 +66,8 @@ class ActionScreenState extends State<ActionScreen> {
                InkWell(
                           onTap: (){
                             window.open('https://apps.apple.com/eg/app/hse-%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D8%AC-%D8%AD%D9%88%D8%A7%D9%81%D8%B2-%D8%A7%D9%84%D8%B3%D9%88%D9%8A%D8%AF%D9%89/id6444411065', 'new tab');
-                            publicScanBloc.add(GetPublicScanData(qr_value: widget.qrCode));
-
-/*                            Navigator.pushAndRemoveUntil(
+                            /*    publicScanBloc.add(GetPublicScanData(qr_value: widget.qrCode));
+                            Navigator.pushAndRemoveUntil(
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) =>
@@ -81,9 +84,8 @@ class ActionScreenState extends State<ActionScreen> {
                 InkWell(
                           onTap: (){
                             window.open('https://play.google.com/store/apps/details?id=com.qara.hse', 'new tab');
-                            publicScanBloc.add(GetPublicScanData(qr_value: widget.qrCode));
-
-                           /* Navigator.pushAndRemoveUntil(
+                            /*    publicScanBloc.add(GetPublicScanData(qr_value: widget.qrCode));
+                            Navigator.pushAndRemoveUntil(
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) =>
@@ -99,30 +101,8 @@ class ActionScreenState extends State<ActionScreen> {
             )
 
 
-            /*Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: ClipOval(
-                  child: Material(
-                    color: Colors.blue, // Button color
-                    child: InkWell(
-                      splashColor: Colors.red, // Splash color
-                      onTap: () {
-                        publicScanBloc.add(GetPublicScanData(qr_value: widget.qrCode));
-
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    ShowDataScreen(qrCode: widget.qrCode)),
-                            ModalRoute.withName("/IntialScreen"));
-                      },
-                      child: SizedBox(
-                          width: 56, height: 56, child: Icon(Icons.ads_click)),
-                    ),
-                  ),
-                )),*/
           ],
-        ),
+        ) : Image.asset("assets/qara_logo.png"),
       ),
     );
   }
